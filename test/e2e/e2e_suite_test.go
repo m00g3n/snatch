e9b8078eb190f536f50460954488cd90f32b3f34
m00g3n <marcin.witalis@gmail.com>
2025-01-01 11:15:16 +0100

    customize github e2e test workflow to install k3d

--- test/e2e/e2e_suite_test.go
+++ test/e2e/e2e_suite_test.go
@@ -43,7 +43,7 @@ var (
 
 	// projectImage is the name of the image which will be build and loaded
 	// with the code source changes to be tested.
-	projectImage = "example.com/kyma-workloads-webhook:v0.0.1"
+	projectImage = "snatch:local"
 )
 
 // TestE2E runs the end-to-end (e2e) test suite for the project. These tests execute in an isolated,
